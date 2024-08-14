3cc983647ce97e525c6b040662e2a2ccc6d720bc
Orion Graciano <87495924+oriongraciano@users.noreply.github.com>
2024-08-13 22:58:23 -0300

    Funcionalidade menu

---
+++ MenuNavbar/scripts.js
@@ -0,0 +1,14 @@
+/*Variaveis*/
+const hamburgerButton = document.querySelector("#hamburgerButton");
+const closeButton = document.querySelector("#closeButton");
+
+const mobileMenu = document.querySelector("#mobileMenu");
+
+/*Eventos*/
+hamburgerButton.addEventListener("click", function () {
+    mobileMenu.classList.add("flex");
+});
+
+closeButton.addEventListener("click", function () {
+    mobileMenu.classList.remove("flex");
+});
